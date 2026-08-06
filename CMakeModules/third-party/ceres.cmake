b08a4d4a9f505e8b3d4ab23a790925da40e5ed56
jcy <2821872892@qq.com>
2026-08-07 07:37:02 +0800

    fix(windows): undefine ERROR before Ceres miniglog severity constants

--- CMakeModules/third-party/ceres.cmake
+++ CMakeModules/third-party/ceres.cmake
@@ -16,9 +16,20 @@ pgo_dep_option(SUITESPARSE BOOL OFF "Use SuiteSparse")
 pgo_dep_option(ACCELERATESPARSE BOOL OFF "Use Apple Accelerate sparse solvers")
 pgo_dep_option(EIGENSPARSE BOOL OFF "Use Eigen sparse solvers")
 
+function(_pgo_patch_ceres_miniglog)
+  # Windows.h defines ERROR as a macro, which breaks miniglog's
+  # "const int ERROR = -2;" severity constant. Undefine it before the header
+  # declares its own constants.
+  pgo_replace_in_file(
+    "${ceres_SOURCE_DIR}/internal/ceres/miniglog/glog/logging.h"
+    "const int ERROR   = -2;"
+    "#ifdef _WIN32\n#undef ERROR\n#endif\nconst int ERROR   = -2;")
+endfunction()
+
 pgo_add_third_party(ceres
   TARGETS Ceres::Ceres
   STATUS "Loading Ceres..."
+  POST_FETCH _pgo_patch_ceres_miniglog
   FETCHCONTENT_ARGS
     URL http://ceres-solver.org/ceres-solver-2.2.0.tar.gz
     EXCLUDE_FROM_ALL
